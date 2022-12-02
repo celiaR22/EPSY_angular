4ea35d7317c4f46aa282390cba4703f7b5bed225
Célia Rojas <celia.rojas@outlook.fr>
2022-12-02 14:45:34 +0100

    rectification bug

--- identity-management/src/app/app-material.module.ts
+++ identity-management/src/app/app-material.module.ts
@@ -1,4 +1,4 @@
-import { NgModule,  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,8 +9,8 @@ import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormField } from '@angular/material/form-field';
-import {MatSlideToggleChange} from '@angular/material/slide-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatInputModule} from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
@@ -30,8 +30,8 @@ import { MatSpinner } from '@angular/material/progress-spinner';
     MatListModule,
     MatTableModule,
     MatPaginatorModule,
-    // MatSlideToggleChange,
-    // MatFormField,
+    MatSlideToggleModule,
+    MatFormFieldModule,
     MatInputModule,
     MatTooltipModule,
     MatProgressSpinnerModule,
@@ -42,6 +42,5 @@ import { MatSpinner } from '@angular/material/progress-spinner';
   providers: [
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue:{duration: 2500, verticalPosition: 'top'}}
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppMaterialModule { }
